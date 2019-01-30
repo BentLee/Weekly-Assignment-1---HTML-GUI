1ee9458211285eeb22b053a937b911d60d9f78a7
BentLee <bnl619@gmail.com>
2019-01-30 11:49:35 -0700

    Add files via upload

---
+++ phone.js
@@ -0,0 +1,22 @@
+$(document).ready(function() { // do this when the document is loaded
+	$("#Dialer").show(); // show the element with ID "element"
+	$("#plusContact").hide(); // hide the element with ID "otherElement"
+	$("#contactList").hide(); // hide the element with ID "otherElement"
+    console.log("heyyyy");
+$("#dialTab").click(function() { // when "button_id" is clicked
+	$("#Dialer").show(); // show element
+	$("#plusContact").hide();	// hide other element
+	$("#contactList").hide();	// hide other element
+
+});
+$("#contactListTab").click(function() { // when "button_id" is clicked
+	$("#contactList").show(); // show element
+	$("#plusContact").hide();	// hide other element
+	$("#Dialer").hide();	// hide other element
+});
+$("#plusContactTab").click(function() { // when "button_id" is clicked
+	$("#plusContact").show(); // show element
+	$("#contactList").hide();	// hide other element
+	$("#Dialer").hide();	// hide other element
+});
+});
